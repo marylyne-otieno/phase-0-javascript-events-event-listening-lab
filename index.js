4461ba360bc6ef8fcac40bb45c3d886581200f04
marylyne <otienomarylyne76@gmail.com>
2025-02-27 16:32:21 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+
+
 function addingEventListener() {
+    const button = document.getElementById('button');
+    button.addEventListener('click', function() {
+        console.log("Button was clicked!");
+    });
 }
